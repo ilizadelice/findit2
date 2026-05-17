2a5fc29526d3e84ee43aed6d46a9320a1aed549a
ilizadelice <ilizadelice@gmail.com>
2026-05-17 01:10:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function eventHTML(title, date, place, cost,englishdescription,kinyarwandadescri
 function showEvents(choice) {
     let output = "";
     const backgrounds = {
-        "Arts and Culture": "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FbE6PUDb4SJ-4u3YrQMVpgpGHcdtXswWxw&s')",
+        "Arts and Culture": "url('https://images.unsplash.com/photo-1569084024058-1632922a4e1d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGFydCUyMGdhbGxlcnl8ZW58MHx8MHx8fDA%3D')",
         "Culture and Traditions": "url('images/culture.jpg')",
         "Music and Nightlife": "url('images/music.jpg')",
         "Sports and Athletics": "url('images/sports.jpg')",
